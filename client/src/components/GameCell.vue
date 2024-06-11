182628a66154040b1472a9a363b754b7ce340c74
justinroche <justinroche03@gmail.com>
2024-06-11 18:52:09 -0500

    Simplify result styles

--- client/src/components/GameCell.vue
+++ client/src/components/GameCell.vue
@@ -18,30 +18,35 @@ if (props.result) {
 <style scoped>
 .card {
   font-weight: 600;
-  border: 3px solid #b6b6b6;
-  border-radius: 5px;
+  font-size: 28px;
+  color: #fff;
+
+  border-radius: 3px;
   box-sizing: border-box;
+
   height: 60px;
   width: 60px;
   margin: 5px;
+
   display: flex;
   align-items: center;
   justify-content: center;
-  font-size: 28px;
+}
+
+.empty {
+  border: 3px solid #b6b6b6;
+  color: #1d1c1c;
 }
 
 .correct {
   background-color: #64b35d;
-  border: 0px;
 }
 
 .misplaced {
   background-color: #d4bc51;
-  border: 0px;
 }
 
 .incorrect {
   background-color: #787c7e;
-  border: 0px;
 }
 </style>
