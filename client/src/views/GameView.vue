c16274f5234410e380577cd265e0464fb760807d
justinroche <justinroche03@gmail.com>
2024-06-18 20:58:36 -0500

    Move game to GameView and add home screen

---
+++ client/src/views/GameView.vue
@@ -0,0 +1,153 @@
+<script setup lang="ts">
+/* Imports */
+import { ref, reactive, onMounted } from 'vue'
+import axios from 'axios'
+import GameTable from '../components/GameTable.vue'
+import Keyboard from '../components/Keyboard.vue'
+import dictionary from '../resources/dictionary.json'
+
+/* State */
+const secretWord = 'MAKER'
+const currentGuess = reactive({
+  row: 0,
+  letters: ['', '', '', '', ''],
+})
+const guesses = ref<string[][]>([])
+const results = ref<string[][]>([])
+const gameState = ref('playing')
+
+const letters: string[][] = [
+  'QWERTYUIOP'.split(''),
+  'ASDFGHJKL'.split(''),
+  'ZXCVBNM'.split(''),
+]
+
+const dictionarySet = new Set(Object.keys(dictionary))
+
+/* Keyboard label state */
+const letterLabels = ref<string[][]>(
+  letters.map((row) => row.map(() => 'unused'))
+)
+
+/* Update keyboard labels */
+function updateLabels(guessedLetters: string[], results: string[]) {
+  guessedLetters.forEach((letter, index) => {
+    const result = results[index]
+
+    for (let i = 0; i < letters.length; i++) {
+      const row = letters[i]
+      const labelRow = letterLabels.value[i]
+      const letterIndex = row.indexOf(letter)
+
+      if (letterIndex !== -1) {
+        const currentLabel = labelRow[letterIndex]
+
+        // Update only if the new label has higher priority
+        if (
+          result === 'correct' ||
+          (result === 'misplaced' && currentLabel !== 'correct') ||
+          (result === 'incorrect' && currentLabel === 'unused')
+        ) {
+          labelRow[letterIndex] = result
+        }
+      }
+    }
+  })
+}
+
+/* Keyboard event handler */
+const handleKeyPress = (event: KeyboardEvent) => {
+  if (currentGuess.row >= 6) return
+  if (gameState.value === 'solved') return
+
+  if (event.key === 'Enter') {
+    if (currentGuess.letters.join('').length !== 5) {
+      return
+    } else {
+      if (!dictionarySet.has(currentGuess.letters.join('').toLowerCase())) {
+        return
+      }
+      if (currentGuess.letters.join('') === secretWord) {
+        gameState.value = 'solved'
+      }
+
+      guesses.value.push([...currentGuess.letters])
+      results.value.push(getResults(currentGuess.letters, secretWord))
+      currentGuess.row++
+      currentGuess.letters = ['', '', '', '', '']
+    }
+  } else if (event.key === 'Backspace') {
+    let lastIndex = currentGuess.letters.findIndex((letter) => letter === '')
+    if (lastIndex === -1) {
+      lastIndex = 5
+    }
+    currentGuess.letters[lastIndex - 1] = ''
+  } else if (
+    /^[a-zA-Z]$/.test(event.key) &&
+    currentGuess.letters.join('').length < 5
+  ) {
+    const index = currentGuess.letters.findIndex((letter) => letter === '')
+    if (index !== -1) {
+      currentGuess.letters[index] = event.key.toUpperCase()
+    }
+  }
+}
+
+/* Submit word event handler */
+function getResults(guess: string[], secretWord: string) {
+  let results: string[] = []
+  let secretWordArr = secretWord.split('')
+
+  // Label correct letters
+  for (let i = 0; i < guess.length; i++) {
+    if (guess[i] === secretWordArr[i]) {
+      results[i] = 'correct'
+      secretWordArr[i] = ''
+    }
+  }
+
+  // Label misplaced letters
+  for (let i = 0; i < guess.length; i++) {
+    if (results[i]) continue
+    if (secretWordArr.includes(guess[i])) {
+      results[i] = 'misplaced'
+      secretWordArr[secretWordArr.indexOf(guess[i])] = ''
+    }
+  }
+
+  // Label incorrect letters
+  for (let i = 0; i < guess.length; i++) {
+    if (!results[i]) {
+      results[i] = 'incorrect'
+    }
+  }
+
+  updateLabels(guess, results)
+
+  return results
+}
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress)
+  testAPI()
+})
+
+async function testAPI() {
+  const response = await axios.get('http://localhost:8080/services/test')
+  console.log(response.data)
+}
+</script>
+
+<template>
+  <h1>Worduel</h1>
+  <game-table :currentGuess="currentGuess" :results="results" />
+  <div id="keyboardContainer">
+    <keyboard id="keyboard" :letters="letters" :letterLabels="letterLabels" />
+  </div>
+</template>
+
+<style scoped>
+#keyboardContainer {
+  margin-top: 100px;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,114 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+const joinCode = ref('')
+
+watch(joinCode, (newValue) => {
+  joinCode.value = newValue.toUpperCase()
+})
+
+const handleHostButton = () => {
+  console.log('Host button clicked')
+}
+
+const handleJoinButton = () => {
+  console.log('Join button clicked')
+}
+</script>
+
+<template>
+  <div class="home-view">
+    <h1>Worduel</h1>
+    <button
+      @click="handleHostButton"
+      class="home-button host-button"
+      tabindex="1"
+    >
+      Host
+    </button>
+    <div class="join-container">
+      <input
+        type="text"
+        class="join-code-input"
+        :class="{ 'join-code-input-spacing': joinCode.length > 0 }"
+        v-model="joinCode"
+        placeholder="Enter code..."
+        maxlength="4"
+        tabindex="2"
+      />
+      <button
+        @click="handleJoinButton"
+        class="home-button join-button"
+        tabindex="3"
+      >
+        Join
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding-bottom: 10vh;
+  box-sizing: border-box;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+.home-button {
+  background-color: #3a3335;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+  height: 40px;
+  transition: background-color 0.05s ease-in-out;
+}
+
+.home-button:hover {
+  background-color: #443b3d;
+}
+
+.home-button:active {
+  background-color: #504346;
+}
+
+.host-button {
+  width: 250px;
+  margin-bottom: 10px;
+}
+
+.join-container {
+  display: flex;
+  justify-content: center;
+  width: 250px;
+  height: 40px;
+}
+
+.join-code-input {
+  border: 1px solid #3a3335;
+  border-radius: 0.25rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+  width: 120px;
+  margin-right: 10px;
+  box-sizing: border-box;
+}
+
+.join-code-input-spacing {
+  letter-spacing: 0.2rem;
+}
+
+.join-button {
+  flex: 1;
+}
+</style>
