85290d6ba6901f34447e32f0f27c05cb602c30e3
justinroche <justinroche03@gmail.com>
2024-06-18 21:00:11 -0500

    Add dotenv and mongoose to server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const PORT = 8080;
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
