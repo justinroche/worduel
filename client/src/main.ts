2aef656e6e637d6479c575cfd91a3795d3f5de66
justinroche <justinroche03@gmail.com>
2024-06-20 19:30:54 -0500

    Install font-awesome icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,4 +3,14 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, fab)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app')
