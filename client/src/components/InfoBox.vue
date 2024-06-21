d218a1e50340b80bcb9d5043618297420fec7df5
justinroche <justinroche03@gmail.com>
2024-06-20 19:31:43 -0500

    Add info box component to home page

---
+++ client/src/components/InfoBox.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="info-box">
+    <div class="info-box-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info-box {
+  background-color: #fff;
+  border: 1px solid #3a3335;
+  border-radius: 0.25rem;
+  padding: 1rem;
+  width: 200px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.info-box-content {
+  font-size: 1rem;
+  color: #000000;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import InfoBox from '../components/InfoBox.vue'
 
 const joinCode = ref('')
+const showInfoBox = ref(false)
 
 // Convert join code to uppercase
 watch(joinCode, (newValue) => {
@@ -22,35 +24,51 @@ const handleHostButton = () => {
 const handleJoinButton = () => {
   console.log('Join button clicked')
 }
+
+const handleInfoButton = () => {
+  showInfoBox.value = !showInfoBox.value
+}
 </script>
 
 <template>
   <div class="home-view">
-    <h1>Worduel</h1>
-    <button
-      @click="handleHostButton"
-      class="home-button host-button"
-      tabindex="1"
-    >
-      Host
-    </button>
-    <div class="join-container">
-      <input
-        type="text"
-        class="join-code-input"
-        :class="{ 'join-code-input-spacing': joinCode.length > 0 }"
-        v-model="joinCode"
-        @input="filterInput"
-        placeholder="Enter code..."
-        maxlength="4"
-        tabindex="2"
-      />
+    <div class="main-content">
+      <h1>Worduel</h1>
       <button
-        @click="handleJoinButton"
-        class="home-button join-button"
-        tabindex="3"
+        @click="handleHostButton"
+        class="home-button host-button"
+        tabindex="1"
       >
-        Join
+        Host
+      </button>
+      <div class="join-container">
+        <input
+          type="text"
+          class="join-code-input"
+          :class="{ 'join-code-input-spacing': joinCode.length > 0 }"
+          v-model="joinCode"
+          @input="filterInput"
+          placeholder="Enter code..."
+          maxlength="4"
+          tabindex="2"
+        />
+        <button
+          @click="handleJoinButton"
+          class="home-button join-button"
+          tabindex="3"
+        >
+          Join
+        </button>
+      </div>
+    </div>
+    <div class="info-section">
+      <transition name="info-box">
+        <info-box v-if="showInfoBox" class="info-box">
+          This is some information.
+        </info-box>
+      </transition>
+      <button @click="handleInfoButton" class="info-button" tabindex="4">
+        <font-awesome-icon :icon="['fas', 'circle-info']" size="2x" />
       </button>
     </div>
   </div>
@@ -63,8 +81,16 @@ const handleJoinButton = () => {
   align-items: center;
   justify-content: center;
   height: 100%;
-  padding-bottom: 10vh;
+  padding-bottom: 15vh;
   box-sizing: border-box;
+  position: relative;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 h1 {
@@ -109,10 +135,8 @@ h1 {
   font-family: inherit;
   font-size: 1rem;
   padding: 0.5rem;
-
   border: 1px solid #3a3335;
   border-radius: 0.25rem;
-
   width: 120px;
   margin-right: 10px;
   box-sizing: border-box;
@@ -124,5 +148,46 @@ h1 {
 
 .join-button {
   flex: 1;
+}
+
+.info-section {
+  position: absolute;
+  bottom: 10vh;
+}
+
+.info-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+}
+
+.info-button:hover .fa-circle-info {
+  color: #443b3d;
+}
+
+.info-box {
+  transform: translateY(-10px);
+  margin-bottom: 10px;
+}
+
+.info-box-enter-to {
+  transition: opacity 0.5s ease, transform 0.3s ease;
+}
+
+.info-box-leave-to {
+  transition: opacity 0.2s ease, transform 0.3s ease;
+}
+
+.info-box-enter-from,
+.info-box-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+.info-box-enter-to,
+.info-box-leave-from {
+  opacity: 1;
+  transform: translateY(-10px);
 }
 </style>
