d6b29775d704692c3f2253df36278ca4379ca0fa
justinroche <justinroche03@gmail.com>
2024-06-29 17:02:43 -0500

    Add loading styles on menu button component

--- client/src/components/MenuButton.vue
+++ client/src/components/MenuButton.vue
@@ -7,24 +7,31 @@ const props = withDefaults(
     buttonHeight: string
     buttonStyle: string
     disabled?: boolean
+    loading?: boolean
   }>(),
   {
     disabled: false,
+    loading: false,
   }
 )
 </script>
 
 <template>
   <button
-    :class="['menu-button', props.buttonStyle]"
+    :class="['menu-button', props.buttonStyle, { loading: props.loading }]"
     :style="{
       width: `${props.buttonWidth}`,
       height: `${props.buttonHeight}`,
       fontSize: `${props.fontSize}`,
     }"
-    :disabled="props.disabled"
+    :disabled="props.disabled || props.loading"
   >
-    {{ buttonText }}
+    <template v-if="props.loading">
+      <span class="loading-spinner"></span>
+    </template>
+    <template v-else>
+      {{ buttonText }}
+    </template>
   </button>
 </template>
 
@@ -38,9 +45,11 @@ const props = withDefaults(
   cursor: pointer;
   box-sizing: border-box;
   transition: background-color 0.1s ease, border 0.1s ease;
+  position: relative;
 }
 
-.menu-button:disabled {
+.menu-button:disabled,
+.menu-button.loading {
   background-color: #ccc;
   color: #666;
   border: 0px;
@@ -48,6 +57,27 @@ const props = withDefaults(
   opacity: 0.6;
 }
 
+.menu-button.loading .loading-spinner {
+  border: 3px solid #f3f3f3;
+  border-top: 3px solid var(--atomic-tangerine-default);
+  border-radius: 50%;
+  width: 12px;
+  height: 12px;
+  animation: spin 2s linear infinite;
+  display: inline-block;
+  vertical-align: middle;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+/* Existing styles for different button styles */
 .atomic-tangerine:not(:disabled) {
   background-color: var(--atomic-tangerine-default);
   border: 1px solid var(--atomic-tangerine-light);
