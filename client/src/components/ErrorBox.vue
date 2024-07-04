845ee4c9649f2f60222fe4225fb17e4a10143593
justinroche <justinroche03@gmail.com>
2024-07-04 16:01:47 -0500

    Add error box component

---
+++ client/src/components/ErrorBox.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const props = defineProps<{ message: string }>()
+</script>
+
+<template>
+  <transition name="fade">
+    <div v-show="props.message" class="error-box">
+      {{ props.message }}
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.error-box {
+  color: white;
+  background-color: rgb(41, 41, 41);
+  padding: 10px;
+  border-radius: 5px;
+  text-align: center;
+  margin-top: 10px;
+  font-size: 0.8rem;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
