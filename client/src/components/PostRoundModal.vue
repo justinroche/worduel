c83b3e7d131609a335101f28e0f2bfe9b967453c
justinroche <justinroche03@gmail.com>
2024-07-10 22:21:46 -0500

    Start building post round modal

---
+++ client/src/components/PostRoundModal.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useSessionStore } from '../stores/SessionStore'
+import GameTable from './GameTable.vue'
+
+const sessionStore = useSessionStore()
+
+const currentRound = computed(() => sessionStore.getCurrentRound)
+</script>
+
+<template>
+  <div class="overlay">
+    <div class="modal">
+      <h2>Round {{ currentRound }} Complete</h2>
+      <div class="result-tables"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+}
+
+.modal {
+  width: 700px;
+  height: 900px;
+  background-color: #fff;
+  border: 1px solid #000;
+  border-radius: 0.25rem;
+}
+</style>
