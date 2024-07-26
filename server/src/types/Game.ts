023d09e8c754cbaa38d426a220c9713d7a398f7f
justinroche <justinroche03@gmail.com>
2024-07-26 14:26:15 -0500

    Remove unnecessary back end types

--- server/src/types/Game.ts
+++
@@ -1,7 +0,0 @@
-import { Word } from './Word'
-
-export type Game = {
-  id: number
-  state: 'setting word' | 'in play' | 'complete'
-  words: Word[]
-}
--- server/src/types/Word.ts
+++
@@ -1,8 +0,0 @@
-export type Word = {
-  wordSetter: 1 | 2
-  word: string
-  guesses: string[]
-  results: string[][]
-  guessingComplete: boolean
-  guessedIn?: number
-}
